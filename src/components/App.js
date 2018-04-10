7ffa98622786e5c57def0931e2cdfc2a65341240
Alec Jandorek <ajandorek@gmail.com>
2018-04-10 15:54:04 -0500

    Added in mets logo and rendered in header

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,6 @@ import React, { Component } from 'react';
 import Header from './Header';
 export default class App extends Component {
   render() {
-    return <Header title="Do the Mets play today?" />;
+    return <Header />;
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import logo from '../images/metslogo.png';
 
 export default class Header extends Component {
   render() {
-    return <h1>{this.props.title}</h1>;
+    return (
+      <h1>
+        Do the <img className="metsLogo" alt="mets logo" src={logo} /> play today?
+      </h1>
+    );
   }
 }
