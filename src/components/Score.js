516713586337329a12d989d899b8ead6fa829f7a
Alec Jandorek <ajandorek@gmail.com>
2018-04-11 14:25:07 -0500

    Have request built out but need to wait for account to be activated

--- src/components/Score.js
+++ src/components/Score.js
@@ -7,12 +7,22 @@ export default class Score extends Component {
   componentDidMount() {
     const todaysDate = moment().format('YYYYMMDD');
     const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
-    axios.get(url).then(res => {
+    axios({
+      type: 'GET',
+      url: url,
+      dataType: 'json',
+      async: false,
+      headers: {
+        Authorization:
+          'Basic ' + btoa(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2)
+      }
+    }).then(res => {
       console.log(res);
     });
   }
 
   render() {
+    console.log(btoa(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2));
     return <div>This is where the score should go.</div>;
   }
 }
