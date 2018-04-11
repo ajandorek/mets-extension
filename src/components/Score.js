9d7ff6257428bd245ff9bd8e56223e5e4d41388c
Alec Jandorek <ajandorek@gmail.com>
2018-04-11 13:35:16 -0500

    Added GET request for daily schedule

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+// import { todaysGame } from '../helpers/getSched';
+import axios from 'axios';
+import moment from 'moment';
 
 export default class Score extends Component {
+  componentDidMount() {
+    const todaysDate = moment().format('YYYYMMDD');
+    const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
+    axios.get(url).then(res => {
+      console.log(res);
+    });
+  }
+
   render() {
     return <div>This is where the score should go.</div>;
   }
--- src/helpers/getSched.js
+++ src/helpers/getSched.js
@@ -1,2 +1,8 @@
 import axios from 'axios';
 import moment from 'moment';
+
+const todaysDate = moment().format('YYYYMMDD');
+const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
+export const todaysGame = axios.get(url).then(res => {
+  console.log(res);
+});
