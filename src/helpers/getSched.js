c023dca068f6d907b632b9c6cd8c79ef2238853e
Alec Jandorek <ajandorek@gmail.com>
2018-04-11 13:57:47 -0500

    Upated request to add in headers

--- src/helpers/getSched.js
+++ src/helpers/getSched.js
@@ -3,6 +3,14 @@ import moment from 'moment';
 
 const todaysDate = moment().format('YYYYMMDD');
 const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
-export const todaysGame = axios.get(url).then(res => {
-  console.log(res);
-});
+export const todaysGame = axios
+  .get({
+    baseURL: url,
+    timeout: 1000,
+    headers: {
+      Authorization: 'Basic ' + btoa(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2)
+    }
+  })
+  .then(res => {
+    console.log(res);
+  });
