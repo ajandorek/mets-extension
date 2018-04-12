eacd11bd7bcda38f33cd994a6c74d6721623b6de
Alec Jandorek <ajandorek@gmail.com>
2018-04-12 18:48:18 -0500

    Moved get request to separate file and have it using Async/Await

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
-// import { todaysGame } from '../helpers/getSched';
-import axios from 'axios';
-import moment from 'moment';
+import { todaysGame } from '../helpers/getSched';
 
 export default class Score extends Component {
   componentDidMount() {
-    const todaysDate = moment().format('YYYYMMDD');
-    const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
-    axios({
-      type: 'GET',
-      url: url,
-      dataType: 'json',
-      async: false,
-      headers: {
-        Authorization:
-          'Basic ' + btoa(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2)
-      }
-    }).then(res => {
-      console.log(res);
-    });
+    todaysGame().then(res => {});
   }
 
   render() {
-    console.log(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2);
     return <div>This is where the score should go.</div>;
   }
 }
--- src/helpers/getSched.js
+++ src/helpers/getSched.js
@@ -3,14 +3,15 @@ import moment from 'moment';
 
 const todaysDate = moment().format('YYYYMMDD');
 const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
-export const todaysGame = axios
-  .get({
-    baseURL: url,
-    timeout: 1000,
+export const todaysGame = async () => {
+  const res = await axios({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    async: false,
     headers: {
       Authorization: 'Basic ' + btoa(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2)
     }
-  })
-  .then(res => {
-    console.log(res);
   });
+  return res;
+};
