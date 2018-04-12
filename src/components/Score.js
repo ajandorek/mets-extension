1db8378e7f9b7b77d167761692f783536d870932
Alec Jandorek <ajandorek@gmail.com>
2018-04-12 18:58:47 -0500

    Added state for result from API and cleaned up console.logs

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import { todaysGame } from '../helpers/getSched';
 
 export default class Score extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      game: false,
+      date: 0,
+      time: 0,
+      homeTeam: '',
+      awayTeam: '',
+      location: ''
+    };
+  }
   componentDidMount() {
-    todaysGame().then(res => {});
+    todaysGame().then(res => {
+      if (res.data.dailygameschedule.gameentry) {
+        this.setState({
+          game: true,
+          date: res.data.dailygameschedule.gameentry[0].date,
+          time: res.data.dailygameschedule.gameentry[0].time,
+          homeTeam: res.data.dailygameschedule.gameentry[0].homeTeam.Abbreviation,
+          awayTeam: res.data.dailygameschedule.gameentry[0].awayTeam.Abbreviation,
+          location: res.data.dailygameschedule.gameentry[0].location
+        });
+      }
+    });
   }
 
   render() {
