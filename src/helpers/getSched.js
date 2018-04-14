2bcb105ecbc4677b7d4c3e2d7d89b89a5cbead61
Alec Jandorek <ajandorek@gmail.com>
2018-04-14 11:08:12 -0500

    Added in request for starting lineups

--- src/helpers/getSched.js
+++ src/helpers/getSched.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import moment from 'moment';
 
 const todaysDate = moment().format('YYYYMMDD');
-const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
+
 export const todaysGame = async () => {
+  const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=${todaysDate}&team=nym`;
   const res = await axios({
     type: 'GET',
     url: url,
@@ -13,5 +14,21 @@ export const todaysGame = async () => {
       Authorization: 'Basic ' + btoa(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2)
     }
   });
+
+  return res;
+};
+
+export const todaysStarters = async gameId => {
+  const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/game_startinglineup.json?gameid=${gameId}`;
+  const res = await axios({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    async: false,
+    headers: {
+      Authorization: 'Basic ' + btoa(process.env.REACT_APP_KEY1 + ':' + process.env.REACT_APP_KEY2)
+    }
+  });
+
   return res;
 };
