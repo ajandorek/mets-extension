713d15a8909eb0f33d6f73b5408c1863f9561614
Alec Jandorek <ajandorek@gmail.com>
2018-05-07 18:17:55 -0500

    Added fade in ability

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import FadeIn from 'react-fade-in';
 
 import Header from './Header';
 import Score from './Score';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <FadeIn>
         <Header />
         <Score />
-      </div>
+      </FadeIn>
     );
   }
 }
